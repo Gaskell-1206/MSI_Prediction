0973aced32126a117a2e263616a0d93a809dc8d5
Shengjia Chen <gaskell1206@hotmail.com>
2022-03-30 22:20:45 -0400

    add PyTorch_lighting_module

--- MIL_train_demo.py
+++ MIL_train_demo.py
@@ -1,217 +1,215 @@
-import sys
 import os
+import urllib.request
+from types import SimpleNamespace
+from urllib.error import HTTPError
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
-import pandas as pd
-from skimage import io
-import argparse
-import random
-import PIL.Image as Image
-from sklearn.preprocessing import LabelEncoder
+import pytorch_lightning as pl
+import seaborn as sns
+import tabulate
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import torch.backends.cudnn as cudnn
-import torch.nn.functional as F
 import torch.utils.data as data
-import torchvision.transforms as transforms
+import torchvision
 import torchvision.models as models
+
+# from IPython.display import HTML, display, set_matplotlib_formats
+from PIL import Image
+from pytorch_lightning.callbacks import LearningRateMonitor, ModelCheckpoint
+from torchvision import transforms
+from typing import Callable, Union, Optional
 
 
 class Args:
-    root_dir = './ex_data/CRC_DX_data_set/'
-    train_lib = './ex_data/CRC_DX_data_set/CRC_DX_Lib/CRC_DX_TRAIN_ALL_Clear.csv'
-    val_lib = './ex_data/CRC_DX_data_set/CRC_DX_Lib/CRC_DX_VAL_ALL.csv'
+    root_dir = './ex_data/CRC_DX_data_set/Dataset'
     output = './ex_data/CRC_DX_data_set/Output'
     batch_size = 512
+    num_workers = 1
     nepochs = 10
-    workers = 1
-    test_every = 10
-    weights = 0.5
-    k = 1
+
+
+
+class MSS_MSI_DataModule(pl.LightningDataModule):
+    def __init__(
+        self,
+        data_path: str,
+        train_transform: Callable,
+        val_transform: Callable,
+        test_transform: Callable,
+        batch_size: int = 1,
+        num_workers: int = 1,
+    ):
+
+        super().__init__()
+        self.data_path = data_path
+        self.train_transform = train_transform
+        self.val_transform = val_transform
+        self.test_transform = test_transform
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+
+    def train_dataloader(self):
+        train_path = os.path.join(self.data_path, "CRC_DX_Train")
+        train_dataset = torchvision.datasets.ImageFolder(train_path, self.train_transform)
+        train_DataLoader = data.DataLoader(train_dataset, batch_size=self.batch_size, shuffle=True,
+                                      num_workers=self.num_workers, pin_memory=True)
+        return train_DataLoader
+
+    def val_dataloader(self):
+        train_path = os.path.join(self.data_path, "CRC_DX_Val")
+        val_dataset = torchvision.datasets.ImageFolder(train_path, self.val_transform)
+        val_DataLoader = data.DataLoader(val_dataset, batch_size=self.batch_size, shuffle=False,
+                                    num_workers=self.num_workers, pin_memory=True)
+        return val_DataLoader
+
+    def test_dataloader(self):
+        train_path = os.path.join(self.data_path, "CRC_DX_Test")
+        test_dataset = torchvision.datasets.ImageFolder(train_path, self.test_transform)
+        test_DataLoader = data.DataLoader(test_dataset, batch_size=self.batch_size, shuffle=False,
+                                     num_workers=self.num_workers, pin_memory=True)
+        return test_DataLoader
+
+
+class MSI_MSSModule(pl.LightningModule):
+    def __init__(self, model_name, model_hparams, optimizer_name, optimizer_hparams):
+        super().__init__()
+
+        self.save_hyperparameters()
+        # self.model = create_model(model_name, model_hparams)
+        self.model=models.resnet18(pretrained=True)
+        self.loss_module = nn.CrossEntropyLoss()
+
+    def forward(self, imgs):
+        return self.model(imgs)
+
+    def configure_optimizers(self):
+        if self.hparams.optimizer_name[0] == "Adam":
+            optimizer = optim.AdamW(self.parameters(),**self.hparams.optimizer_hparams[0])
+        elif self.hparams.optimizer_name[0] == "SGD":
+            optimizer = optim.SGD(self.parameters(), **self.hparams.optimizer_hparams[0])
+        else:
+            assert False, f'Unknown optimizer: "{self.hparams.optimizer_name}"'
+        # Reduce the learning rate by 0.1 after 100 and 150 epochs
+        scheduler = optim.lr_scheduler.MultiStepLR(
+            optimizer, milestones=[100, 150], gamma=0.1)
+        return [optimizer], [scheduler]
+
+    def training_step(self, batch, batch_idx):
+        imgs, labels = batch
+        preds = self.model(imgs)
+        loss = self.loss_module(preds, labels)
+        acc = (preds.argmax(dim=-1) == labels).float().mean()
+        # Logs the accuracy per epoch to tensorboard (weighted average over batches)
+        self.log("train_acc", acc, on_step=False, on_epoch=True)
+        self.log("train_loss", loss)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx):
+        imgs, labels = batch
+        preds = self.model(imgs).argmax(dim=-1)
+        acc = (labels == preds).float().mean()
+        # By default logs it per epoch (weighted average over batches)
+        self.log("val_acc", acc)
+
+    def test_step(self, batch, batch_idx):
+        imgs, labels = batch
+        preds = self.model(imgs).argmax(dim=-1)
+        acc = (labels == preds).float().mean()
+        # By default logs it per epoch (weighted average over batches), and returns it afterwards
+        self.log("test_acc", acc)
+
+
+def create_model(model_name, model_hparams):
+    model_dict = {}
+    if model_name in model_dict:
+        return model_dict[model_name](**model_hparams)
+    else:
+        assert False, f'Unknown model name "{model_name}". Available models are: {str(model_dict.keys())}'
 
 
 def main():
-    global args, best_acc
-    # args = parser.parse_args()
     args = Args()
 
-    # define model cnn
-    model = models.resnet18(pretrained=True)
-    num_ftrs = model.fc.in_features
-    # model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-    model.fc = nn.Linear(num_ftrs, 2)
+    #Environment
+    DATASET_PATH = os.environ.get("PATH_DATASETS", "data/")
+    CHECKPOINT_PATH = os.environ.get("PATH_CHECKPOINT", "saved_models/ConvNets")
+    pl.seed_everything(42)
 
-    device = torch.device(
-        "cuda") if torch.cuda.is_available() else torch.device("cpu")
-    print(device)
+    torch.backends.cudnn.determinstic = True
+    torch.backends.cudnn.benchmark = False
 
-    criterion = nn.CrossEntropyLoss().to(device)
-    optimizer = optim.Adam(model.parameters(), lr=1e-4, weight_decay=1e-4)
-    cudnn.benchmark = True
+    device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 
-    # normalization
-    normalize = transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.1, 0.1, 0.1])
-    trans = transforms.Compose([transforms.ToTensor(), normalize])
+    # data
+    train_dataset = torchvision.datasets.ImageFolder(os.path.join(args.root_dir,"CRC_DX_Train"))
+    # DATA_MEANS = (train_dataset.data / 255.0).mean(axis=(0, 1, 2))
+    # DATA_STD = (train_dataset.data / 255.0).std(axis=(0, 1, 2))
+    DATA_MEANS = [0.5,0.5,0.5]
+    DATA_STD = [0.1,0.1,0.1]
+    print("Data mean", DATA_MEANS)
+    print("Data std", DATA_STD)
+    train_transform = transforms.Compose(
+        [
+            transforms.RandomHorizontalFlip(),
+            transforms.ToTensor(),
+            transforms.Normalize(DATA_MEANS, DATA_STD)
+        ]
+    )
+    test_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize(DATA_MEANS, DATA_STD)])
 
-    # load data
-    train_dset = MILdataset(args.train_lib, args.root_dir, trans)
-    # print(next(iter(train_dset)))
-    train_loader = torch.utils.data.DataLoader(
-        train_dset,
-        batch_size=args.batch_size, shuffle=True,
-        num_workers=args.workers, pin_memory=False)
-    if args.val_lib:
-        val_dset = MILdataset(args.val_lib, args.root_dir, trans)
-        val_loader = torch.utils.data.DataLoader(
-            val_dset,
-            batch_size=args.batch_size, shuffle=False,
-            num_workers=args.workers, pin_memory=False)
+    data_module = MSS_MSI_DataModule(
+        data_path=args.root_dir,
+        train_transform=train_transform,
+        val_transform=test_transform,
+        test_transform=test_transform,
+        batch_size=args.batch_size,
+        num_workers=args.num_workers,
+    )
 
-    # open output file
-    fconv = open(os.path.join(args.output, 'convergence.csv'), 'w')
-    fconv.write('epoch,metric,value\n')
-    fconv.close()
+    # model
+    model_name = "resnet18"
+    model_hparams={"num_classes": 2, "act_fn_name": "relu"}
+    optimizer_name="Adam",
+    optimizer_hparams={"lr": 1e-3, "weight_decay": 1e-4},
+    model = MSI_MSSModule(model_name, model_hparams, optimizer_name, optimizer_hparams)
 
-    # loop through epochs
-    for epoch in range(args.nepochs):
-        # train
-        loss = train(epoch, train_loader, model, criterion, optimizer, device)
-        print(
-            'Training\tEpoch: [{}/{}]\tLoss: {}'.format(epoch+1, args.nepochs, loss))
-        fconv = open(os.path.join(args.output, 'convergence.csv'), 'a')
-        fconv.write('{},loss,{}\n'.format(epoch+1, loss))
-        fconv.close()
+    # training
+    
+    save_name = model_name
 
-        # validation
-        if args.val_lib:
-            pred = inference(epoch, val_loader, model, device)
-            err, fpr, fnr = calc_err(pred, val_dset.targets)
-            print('Validation\tEpoch: [{}/{}]\tError: {}\tFPR: {}\tFNR: {}'.format(
-                epoch+1, args.nepochs, err, fpr, fnr))
-            fconv = open(os.path.join(args.output, 'convergence.csv'), 'a')
-            fconv.write('{},error,{}\n'.format(epoch+1, err))
-            fconv.write('{},fpr,{}\n'.format(epoch+1, fpr))
-            fconv.write('{},fnr,{}\n'.format(epoch+1, fnr))
-            fconv.close()
-            # Save best model
-            err = (fpr+fnr)/2.
-            if 1-err >= best_acc:
-                best_acc = 1-err
-                obj = {
-                    'epoch': epoch+1,
-                    'state_dict': model.state_dict(),
-                    'best_acc': best_acc,
-                    'optimizer': optimizer.state_dict()
-                }
-                torch.save(obj, os.path.join(
-                    args.output, 'checkpoint_best.pth'))
+    # Create a PyTorch Lightning trainer with the generation callback
+    trainer = pl.Trainer(
+        default_root_dir=os.path.join(CHECKPOINT_PATH, save_name),
+        gpus=1 if str(device) == "cuda:0" else 0,
+        min_epochs=10,
+        max_epochs=100,
+        callbacks=[ModelCheckpoint(save_weights_only=True, mode="max", monitor="val_acc"), 
+        LearningRateMonitor("epoch")],
+        auto_scale_batch_size='binsearch', # [Optional] auto_scale_batch_size
+        auto_lr_find=True
+    ) 
+    trainer.logger._log_graph = True  # If True, we plot the computation graph in tensorboard
+    trainer.logger._default_hp_metric = None  # Optional logging argument that we don't need
 
+    # [Optional] lr_finder
+    lr_finder = trainer.tuner.lr_find(model,datamodule=data_module)
+    fig = lr_finder.plot(suggest=True)
+    fig.show()
 
-class MILdataset(data.Dataset):
-    def __init__(self, libraryfile='', root_dir='', transform=None):
+    model.hparams.learning_rate = lr_finder.suggestion()
 
-        lib = pd.read_csv(libraryfile)
-        lib = lib.sort_values(['subject_id'], ignore_index=True)
-        # slides = []
+    # fit
+    trainer.fit(model, datamodule=data_module)
 
-        # for i, name in enumerate(lib['subject_id'].unique()):
-        #     sys.stdout.write(
-        #         'Slides: [{}/{}]\r'.format(i+1, len(lib['subject_id'].unique())))
-        #     sys.stdout.flush()
-        #     slides.append(name)
-
-        # grid = []
-        # slideIDX = []
-        # for i, g in enumerate(lib['subject_id'].unique()):
-        #     tiles = lib[lib['subject_id'] == g]['slice_id']
-        #     grid.extend(tiles)
-        #     slideIDX.extend([i]*len(tiles))
-
-        # print('Number of tiles: {}'.format(len(grid)))
-        self.dataframe = load_data_and_get_class(libraryfile)
-        self.slidenames = list(lib['subject_id'].values)
-        # self.slides = slides
-        self.targets = list(lib['label'].values)
-        # self.grid = grid
-        # self.slideIDX = slideIDX
-        self.transform = transform
-        self.root_dir = root_dir
-        if "TRAIN" in libraryfile:
-            self.mode = "CRC_DX_Train"
-        elif "VAL" in libraryfile:
-            self.mode = "CRC_DX_Val"
-        else:
-            self.mode = "CRC_DX_Test"
-
-    def __getitem__(self, index):
-
-        # slideIDX = self.slideIDX[index]
-        # tile_id = self.grid[index]
-        # slide_id = self.slides[slideIDX]
-        # img_name = "blk-{}-{}.png".format(tile_id, slide_id)
-        img_name = self.dataframe.iloc[index, 3]
-        target = self.dataframe.iloc[index, -1]
-
-        label = 'CRC_DX_MSIMUT' if target == 0 else 'CRC_DX_MSS'
-
-        img_path = os.path.join(self.root_dir, self.mode, label, img_name)
-        img = io.imread(img_path)
-
-        if self.transform is not None:
-            img = self.transform(img)
-        return (img, target)
-
-    def __len__(self):
-        return len(self.dataframe)
+    # Test best model on test set
+    model = MSI_MSSModule.load_from_checkpoint(trainer.checkpoint_callback.best_model_path) 
+    test_result = trainer.test(model, datamodule=data_module, verbose=True)
+    result = {"test": test_result[0]["test_acc"]}
+    print(result)
 
 
-def inference(run, loader, model, device):
-    model.eval()
-    model = model.to(device)
-    probs = torch.FloatTensor(len(loader.dataset))
-    with torch.no_grad():
-        for i, input in enumerate(loader):
-            print(
-                'Inference\tEpoch: [{}/{}]\tBatch: [{}/{}]'.format(run+1, args.nepochs, i+1, len(loader)))
-            input = input.to(device)
-            output = F.softmax(model(input), dim=1)
-            # probs[i*args.batch_size:i*args.batch_size+input.size(0)] = output.detach()[:,1].clone()
-    return output.cpu().numpy()
-
-
-def train(run, loader, model, criterion, optimizer, device):
-    model.train()
-    running_loss = 0.0
-    model = model.to(device)
-    for i, (input, target) in enumerate(loader):
-        input = input.to(device)
-        target = target.to(device)
-        output = model(input)
-        loss = criterion(output, target)
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-        running_loss += loss.item()*input.size(0)
-    return running_loss/len(loader.dataset)
-
-
-def calc_err(pred, real):
-    pred = np.array(pred)
-    real = np.array(real)
-    neq = np.not_equal(pred, real)
-    err = float(neq.sum())/pred.shape[0]
-    fpr = float(np.logical_and(pred == 1, neq).sum())/(real == 0).sum()
-    fnr = float(np.logical_and(pred == 0, neq).sum())/(real == 1).sum()
-    return err, fpr, fnr
-
-
-def load_data_and_get_class(path_to_data):
-    data = pd.read_csv(path_to_data)
-    encoder = LabelEncoder()
-    encoder.fit(["MSI","MSS"])
-    data['Class'] = encoder.transform(data['label'])
-    return data
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
